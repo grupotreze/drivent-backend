031972dc5021fa6111623a22520c94652d9dba0e
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-08-11 16:29:40 -0300

    feat: gitHub Login

--- src/controllers/authentication-controller.ts
+++ src/controllers/authentication-controller.ts
@@ -1,14 +1,26 @@
 import authenticationService, { SignInParams } from "@/services/authentication-service";
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+
 export async function singInPost(req: Request, res: Response) {
   const { email, password } = req.body as SignInParams;
 
   try {
     const result = await authenticationService.signIn({ email, password });
-
     return res.status(httpStatus.OK).send(result);
   } catch (error) {
+    console.log(error);
+    return res.status(httpStatus.UNAUTHORIZED).send({});
+  }
+}
+
+export async function loginOauth(req: Request, res: Response) {
+  const code = req.body.code as string;
+  try {
+    const result = await authenticationService.oauthLogin(code);
+    return res.status(httpStatus.OK).send(result);
+  } catch (error) {
+    console.log(error);
     return res.status(httpStatus.UNAUTHORIZED).send({});
   }
 }
